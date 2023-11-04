75247f2da857035a1406d116e9dba4590a0b5dab
TC <tomasz.cichacki@gmail.com>
2023-11-04 14:39:30 +0100

    modified:   srcdev/run.sh

--- srcdev/run.sh
+++ srcdev/run.sh
@@ -2,7 +2,7 @@
 
 export USERNAME=$(bashio::config 'energa_username')
 export PASSWORD=$(bashio::config 'energa_password')
-export LOGLEVEL=$(bashio::config 'log_level')
+export LOG_LEVEL=$(bashio::config 'log_level')
 
 bashio::log.info "Uruchamiam API"
 python run.py
