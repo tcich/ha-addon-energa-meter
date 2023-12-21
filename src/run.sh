7f4042f3ac92d6042d8145f4f2ffa68cf3ed2800
TC <tomasz.cichacki@gmail.com>
2023-12-21 23:09:48 +0100

    new file:   requirements.txt 	modified:   src/Dockerfile 	modified:   src/config.yaml 	modified:   src/run.sh

--- src/run.sh
+++ src/run.sh
@@ -5,5 +5,6 @@ export PASSWORD=$(bashio::config 'energa_password')
 export LOG_LEVEL=$(bashio::config 'log_level')
 
 bashio::log.info "Uruchamiam API"
+source /app/venv/bin/activate
 python run.py
 
